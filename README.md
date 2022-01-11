852f261082f84aa84e5db7bc53acb5c23b006861
Ville Brofeldt <ville.v.brofeldt@gmail.com>
2022-01-11 18:27:58 +0200

    initial commit
    add image
    
    simplify deps
    
    add more suff to .gitignore
    
    update readme

---
+++ README.md
@@ -0,0 +1,42 @@
+## superset-plugin-chart-liquid
+
+This plugin provides an example chart plugin for Superset to visualize percentages. The plugin has been built using the [Superset Yeoman Generator](https://www.npmjs.com/package/@superset-ui/generator-superset) and leverages the [And Design Charts](https://charts.ant.design) charting library.
+
+![Liquid chart plugin in Explore view](images/liquid.png)
+
+### Usage
+
+To build the plugin, run the following commands:
+
+```
+npm ci
+npm run build
+```
+
+To run the plugin in development mode (=rebuilding whenever changes are made), start the dev server with the following command:
+
+```
+npm run dev
+```
+
+To add the package to Superset, go to the `superset-frontend` subdirectory in your Superset source folder (assuming both the `superset-plugin-chart-liquid` plugin and `superset` repos are in the same root directory) and run
+```
+npm i -S ../../superset-plugin-chart-liquid
+```
+
+After this edit the `superset-frontend/src/visualizations/presets/MainPreset.js` and make the following changes:
+
+```js
+import { LiquidChartPlugin } from 'superset-plugin-chart-liquid';
+```
+
+to import the plugin and later add the following to the array that's passed to the `plugins` property:
+```js
+new LiquidChartPlugin().configure({ key: 'liquid' }),
+```
+
+After that the plugin should show up when you run Superset, e.g. the development server:
+
+```
+npm run dev-server
+```
---
+++ src/Liquid.tsx
@@ -0,0 +1,60 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+import React, { createRef, useMemo } from 'react';
+import { styled } from '@superset-ui/core';
+import { Liquid as AntvLiquid } from '@ant-design/plots';
+
+import { LiquidProps, LiquidStylesProps } from './types';
+const Styles = styled.div<LiquidStylesProps>`
+  height: ${({ height }) => height};
+  width: ${({ width }) => width};
+`;
+
+export default function Liquid(props: LiquidProps) {
+  const { shape, percentage, height, width } = props;
+
+  const rootElem = createRef<HTMLDivElement>();
+
+  const config = useMemo(() => ({
+      percent: percentage,
+      outline: {
+        border: 2,
+        distance: 4,
+      },
+      shape,
+      wave: {
+        length: 128,
+      },
+      width,
+      height,
+      autoFit: false,
+    }),
+    [height, width, percentage],
+  );
+
+  return (
+    <Styles
+      ref={rootElem}
+      height={height}
+      width={width}
+    >
+      <AntvLiquid {...config} />
+    </Styles>
+  );
+}
