697c8ee2648ab736b255152bb7171dea93cf579b
Ville Brofeldt <ville.v.brofeldt@gmail.com>
2022-01-26 17:41:22 +0200

    remove redundant groupby

--- src/plugin/buildQuery.ts
+++ src/plugin/buildQuery.ts
@@ -19,11 +19,9 @@
 import { buildQueryContext, QueryFormData } from '@superset-ui/core';
 
 export default function buildQuery(formData: QueryFormData) {
-  const { cols: groupby } = formData;
   return buildQueryContext(formData, baseQueryObject => [
     {
       ...baseQueryObject,
-      groupby,
     },
   ]);
 }
